ff8169c7843381faa434836da4c309552009366c
Mark Bob <42984162+MarktheBob@users.noreply.github.com>
2018-12-15 17:10:10 -0500

    Add files via upload

---
+++ scripts/hider.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.small').toggleClass('hidden');
+    $('#hider').click(function(){
+        $('#jssor_1').toggleClass('hidden');
+        $('.small').toggleClass('hidden');
+    })
+})
